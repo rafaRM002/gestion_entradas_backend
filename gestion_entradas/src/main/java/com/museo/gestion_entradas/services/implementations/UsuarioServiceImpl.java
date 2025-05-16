c859ce4aa618abe6f8abbbbf8f55b8968d58fb92
rafaRM002 <156197209+rafaRM002@users.noreply.github.com>
2025-05-16 14:29:41 +0200

    Actualizacion de Service e Implementations

---
+++ gestion_entradas/src/main/java/com/museo/gestion_entradas/services/implementations/UsuarioServiceImpl.java
@@ -0,0 +1,36 @@
+package com.museo.gestion_entradas.services.implementations;
+
+import com.museo.gestion_entradas.entities.Usuario;
+import com.museo.gestion_entradas.repository.UsuarioRepository;
+import com.museo.gestion_entradas.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("usuarioServiceImpl")
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public List<Usuario> ListarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Usuario crearUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Usuario actualizarUsuario(Usuario usuarioExistente) {
+        return usuarioRepository.save(usuarioExistente);
+    }
+
+    @Override
+    public void EliminarUsuario(int id) {usuarioRepository.deleteById(id);}
+
+}
+
