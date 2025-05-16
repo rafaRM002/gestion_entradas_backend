698671f0b787dce6460aa741efbdde4f1dbe7d51
rafaRM002 <156197209+rafaRM002@users.noreply.github.com>
2025-05-16 14:22:05 +0200

    Añadido usuarioservice

---
+++ gestion_entradas/src/main/java/com/museo/gestion_entradas/services/UsuarioService.java
@@ -0,0 +1,35 @@
+package com.museo.gestion_entradas.services;
+
+import com.museo.gestion_entradas.entities.Usuario;
+
+import java.util.List;
+
+public interface UsuarioService {
+
+    /**
+     * Metodo que Lista todos los usuarios
+     * @return retorna una lista de usuarios
+     */
+    List<Usuario> ListarUsuarios();
+
+    Integer listarUsuariosActivos();
+
+    /**
+     * Se encarga de Listar un usuario en concreto
+     * @Param recibe un usuario para su listado
+     * @return retorna el usuario que quermos ver
+     */
+    Usuario CrearActualizarUsuario(Usuario usuario);
+
+    Usuario actualizarUsuario(Usuario usuario);
+    /**
+     * Se encarga de eliminar un uusuario recibiendo los datos
+     * @Param recibe un usuario para su eliminacion
+     */
+    void EliminarUsuario(int id);
+
+    Usuario BuscarUsuario(int id);
+
+    Boolean ExisteUsuario(String email);
+
+}
