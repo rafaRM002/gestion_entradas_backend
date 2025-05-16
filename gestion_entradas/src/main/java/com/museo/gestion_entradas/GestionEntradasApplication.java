ceff010474c61a6881ac5e57d0038c704503cb5e
rafaRM002 <156197209+rafaRM002@users.noreply.github.com>
2025-05-16 13:56:22 +0200

    Creación de proyecto
    Nuevo proyecto gestion_entradas_museo

---
+++ gestion_entradas/src/main/java/com/museo/gestion_entradas/GestionEntradasApplication.java
@@ -0,0 +1,13 @@
+package com.museo.gestion_entradas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEntradasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEntradasApplication.class, args);
+	}
+
+}
