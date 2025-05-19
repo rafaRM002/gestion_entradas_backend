0642223a96e6b5916a9c752ccf28ab49b4241e22
manuelgonz <manuel5365.mgl@gmail.com>
2025-05-19 08:37:01 +0200

    Producto Controller

--- gestion_entradas/src/main/java/com/museo/gestion_entradas/controller/UsuarioController.java
+++ gestion_entradas/src/main/java/com/museo/gestion_entradas/controller/UsuarioController.java
@@ -1,4 +1,13 @@
 package com.museo.gestion_entradas.controller;
 
+import com.museo.gestion_entradas.entities.Producto;
+import com.museo.gestion_entradas.entities.Usuario;
+import com.museo.gestion_entradas.services.implementations.ProductoServiceImpl;
+import com.museo.gestion_entradas.services.implementations.UsuarioServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class UsuarioController {
+
 }
