39bc0a8726ad8dd39a614f762c66f7efed2d400f
manuelgonz <manuel5365.mgl@gmail.com>
2025-05-19 08:29:41 +0200

    Inserccion de LineaVenta y ProductoService

--- gestion_entradas/src/main/java/com/museo/gestion_entradas/services/ProductoService.java
+++ gestion_entradas/src/main/java/com/museo/gestion_entradas/services/ProductoService.java
@@ -1,4 +1,28 @@
 package com.museo.gestion_entradas.services;
 
+import com.museo.gestion_entradas.entities.Producto;
+import com.museo.gestion_entradas.entities.Usuario;
+
+import java.util.List;
+
 public interface ProductoService {
+    /**
+     * Metodo que Lista todos los usuarios
+     * @return retorna una lista de usuarios
+     */
+    List<Producto> listarProductos();
+
+    /**
+     * Se encarga de Listar un usuario en concreto
+     * @Param recibe un usuario para su listado
+     * @return retorna el usuario que quermos ver
+     */
+    Producto crearProducto(Producto producto);
+
+    Producto actualizarProducto(Producto producto);
+    /**
+     * Se encarga de eliminar un uusuario recibiendo los datos
+     * @Param recibe un usuario para su eliminacion
+     */
+    void eliminarProducto(int id);
 }
